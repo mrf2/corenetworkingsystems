03afd29b9b9b24ebf477fac848b285fdce7e1fa6
mrf2 <mrf2.github@gmail.com>
2025-07-21 08:15:00 +0600

    update open_clientfd.c

--- sockif/open_clientfd.c
+++ sockif/open_clientfd.c
@@ -1,19 +1,25 @@
 /*
+ * filename: open_clientfd.c
  * typedef sturct sockaddr SA;
  */
+
+#include <strings.h>
 
 int open_clientfd(char *hostname, int port)
 {
 	int clientfd;
 	struct hostent *hp;
 	struct sockaddr_in serveraddr;
-
+     /* int socket(int domain, int type, int protocol); */
 	if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return -1;	/* Check error for cause of error */
 	
 	/* Fill in the server's IP address and port */
+      /* int gethostname(char *name, size_t len); */
 	if ((hp = gethostbyname(hostname)) == NULL)
 		return -2;	/* Check h_error for cause of error */
+	
+   /* void bzero(void s[.n], size_t n); */
 	bzero((char *)&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
 	bcopy((char *)hp->h_addr_list[0],
